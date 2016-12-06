d3349a3e4fa535ca84f5cc333102bda2f1c9e93d
goncho314 <Gonzalo.Garcia8@alu.uclm.es>
2016-12-06 17:31:26 +0100

    Reinicio del repositorio

--- SoftCorpESI/SoftCorpESI/src/edu/uclm/esi/iso2/multas/domain/PruebaPush.java
+++
@@ -1,7 +0,0 @@
-package edu.uclm.esi.iso2.multas.domain;
-
-public class PruebaPush {
-	public static void main (String [] args){
-		System.out.println("Esto es una prueba de commit");
-	}
-}
--- SoftCorpESI/SoftCorpESI/src/edu/uclm/esi/iso2/multas/domain/a.java
+++
@@ -1,6 +0,0 @@
-package edu.uclm.esi.iso2.multas.domain;
-
-public class a {
-	private int a;
-
-}
